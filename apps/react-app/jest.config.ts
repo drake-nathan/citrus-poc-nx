fc683335f8fde30d84db5023438a6f209fae36cd
Nathan Drake <ndrake@paylocity.com>
2024-02-14 16:48:20 -0600

    finish eslint

--- apps/react-app/jest.config.ts
+++ apps/react-app/jest.config.ts
@@ -1,11 +1,10 @@
-/* eslint-disable */
 export default {
-  displayName: "citrus-poc-nx",
+  displayName: "react-app",
   preset: "../../jest.preset.js",
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
     "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/apps/citrus-poc-nx",
+  coverageDirectory: "../../coverage/apps/react-app",
 };
--- apps/react-app/src/app/app.spec.tsx
+++ apps/react-app/src/app/app.spec.tsx
@@ -5,11 +5,7 @@ import App from "./app";
 describe("App", () => {
   it("should render successfully", () => {
     const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
-  });
 
-  it("should have a greeting as the title", () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome citrus-poc-nx/gi)).toBeTruthy();
+    expect(baseElement).toBeTruthy();
   });
 });
