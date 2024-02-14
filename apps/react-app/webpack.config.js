f91b1cc1cb63cd42a80afbdd5fb694bfe9ef5f78
Nathan Drake <ndrake@paylocity.com>
2024-02-14 16:16:46 -0600

    setup and run prettier

--- apps/react-app/webpack.config.js
+++ apps/react-app/webpack.config.js
@@ -1,25 +1,25 @@
-const { NxWebpackPlugin } = require('@nx/webpack');
-const { NxReactWebpackPlugin } = require('@nx/react');
-const { join } = require('path');
+const { NxWebpackPlugin } = require("@nx/webpack");
+const { NxReactWebpackPlugin } = require("@nx/react");
+const { join } = require("path");
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/citrus-poc-nx'),
+    path: join(__dirname, "../../dist/apps/citrus-poc-nx"),
   },
   devServer: {
     port: 4200,
   },
   plugins: [
     new NxWebpackPlugin({
-      tsConfig: './tsconfig.app.json',
-      compiler: 'babel',
-      main: './src/main.tsx',
-      index: './src/index.html',
-      baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
-      styles: ['./src/styles.css'],
-      outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
-      optimization: process.env['NODE_ENV'] === 'production',
+      tsConfig: "./tsconfig.app.json",
+      compiler: "babel",
+      main: "./src/main.tsx",
+      index: "./src/index.html",
+      baseHref: "/",
+      assets: ["./src/favicon.ico", "./src/assets"],
+      styles: ["./src/styles.css"],
+      outputHashing: process.env["NODE_ENV"] === "production" ? "all" : "none",
+      optimization: process.env["NODE_ENV"] === "production",
     }),
     new NxReactWebpackPlugin({
       // Uncomment this line if you don't want to use SVGR
